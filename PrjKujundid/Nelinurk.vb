4193a146ec98b695812717549791384d5f29e576
vevilt <vevilt@ICT-50120K>
2024-02-28 17:04:26 +0200

    added class nelinurk

---
+++ PrjKujundid/Nelinurk.vb
@@ -0,0 +1,25 @@
+﻿Public MustInherit Class Nelinurk
+    Inherits Kujund
+
+    Protected kylgD As Double
+
+    Public Sub New(ByVal tyyp As String, ByVal kylgA As Double, ByVal kylgB As Double,
+                   ByVal kylgC As Double, ByVal kylgD As Double)
+
+        MyBase.New(tyyp, kylgA, kylgB, kylgC)
+
+        Me.kylgD = kylgD
+
+    End Sub
+
+    Public Overrides Function leiaYmberMoot() As Double
+
+        Return kylgA + kylgB + kylgC + kylgD
+
+    End Function
+    Public Overrides Function leiaPindala() As Double
+
+        Return 0
+
+    End Function
+End Class
